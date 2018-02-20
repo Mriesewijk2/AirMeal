3bbec3b1f90528e75b5e850d5c55510282dc189d
Mriesewijk2 <m.riesewijk@student.utwente.nl>
2018-02-20 11:30:41 +0100

    second commit

---
+++ src/com/srccodes/example/HelloWorld.java
@@ -0,0 +1,46 @@
+package com.srccodes.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    int count;  
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        count = 12;
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter printWriter  = response.getWriter();
+        int count2 = 5;
+        printWriter.println("<h1>Hello World!</h1>" + count + " " + count2);
+    }
+ 
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
